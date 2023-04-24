2651d64b200bd78096a60ff13204ba82e98d01f6
BlossomPB <113061170+BlossomPB@users.noreply.github.com>
2023-04-24 00:39:50 -0700

    Update mazePositions.h

--- mazePositions.h
+++ mazePositions.h
@@ -23,14 +23,45 @@ struct positionMaker {
     bool hasCrystal;
 };
 
-class posInfo {
-public:
-    string posNumber;
-    string nextDirection;
-    string figureNextPos(string posNumber, string nextDirection);
-};
 
 
-void randSpace(int* randSpace);
+
+//\\// maze generation functions //\\//
+
+// First layer maker functions
+    void createFirstLayer();
+    void organizeFirstLayer();
+
+// RNG for Maze Maker functions
+    void randSpace();
+    void randNumOfRow();
+    void oneOrZeroFunc();
+    void oneToThreeFunc();
+    void oneToFourFunc();
+
+// Maze Maker function helpers
+    void makeCorner(int r, int c);
+    void makeLeft(int r, int c);
+    void makeRight(int r, int c);
+    void makeDown(int r, int c);
+
+// Organize Maze function helpers
+    void checkUpLeft(int r, int c);
+    void checkUpRight(int r, int c);
+    void checkDownLeft(int r, int c);
+    void checkDownRight(int r, int c);
+
+// Maze Generator functions
+    void generateRestOfMaze();
+    void createInterectionPoints();
+    void organizeIntersections();
+    void setStartandEnd();
+    void setCrystal();
+
+//   END   \\// *** //\\   END   //
+
+// Print maze function
+    void printMaze();
+
 void startMaze(int* currPosptr,string* directionPtr,bool* dirValPtr);
 void validateDirection(string* directionPtr, bool* dirValPtr);
